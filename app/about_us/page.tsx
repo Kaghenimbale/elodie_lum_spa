3eb72c3cd2ead140f9aee755695af202f852f880
Kagheni Mbale <kaghenimbale@gmail.com>
2025-07-09 07:14:21 +0200

    Add modification to the about page

--- app/about_us/page.tsx
+++ app/about_us/page.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-
 const page = () => {
   return (
-    <div className="w-[100%] pt-28 bg-red-500">
-      THIS IS THE ABOUT PAGE
-      <div className="w-[100%] h-[50rem]"></div>
-    </div>
+    <div className="w-[100%] pt-28 bg-red-500">THIS IS THE ABOUT PAGE</div>
   );
 };
 
