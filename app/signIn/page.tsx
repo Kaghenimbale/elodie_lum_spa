631800afb9b031eea9c0849db7676037086ddbff
Kagheni Mbale <kaghenimbale@gmail.com>
2025-07-12 21:20:15 +0200

    Add modification style to the sign in sign up pages

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -23,28 +23,38 @@ const page = () => {
   };
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center">
-      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <h2 className="text-[1.8rem] md:text-[2rem] font-thin">Sign In</h2>
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 bg-slate-100 shadow-sm shadow-slate-100 p-4"
+      >
+        <h2 className="text-[1.8rem] md:text-[2rem] font-bold">Sign In</h2>
 
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="email"
-          placeholder="Email address"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-          required
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="email"
+            placeholder="mail@example.com"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={data.password}
-          onChange={handleChange}
-          required
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password">Password:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
         <button
           type="submit"
           className="text-white text-[0.9rem] bg-cyan-800 w-fit hover:bg-cyan-700 transition-all duration-300 ease-in-out"
@@ -52,7 +62,7 @@ const page = () => {
           Sign In
         </button>
         <span className="flex gap-2">
-          Don't have an account ?
+          If you don't have an account, Please
           <Link href="/signUp" className="text-blue-700 underline">
             Sign Up
           </Link>
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -27,47 +27,63 @@ const page = () => {
   };
   return (
     <div className="w-[100vw] h-[100vh] flex items-center justify-center">
-      <form action="" onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <h2 className="text-[1.8rem] md:text-[2rem] font-thin">Sign Up</h2>
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="name"
-          placeholder="Name"
-          name="name"
-          value={data.name}
-          onChange={handleChange}
-          required
-        />
+      <form
+        action=""
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 bg-slate-100 shadow-sm shadow-slate-100 p-4"
+      >
+        <h2 className="text-[1.8rem] md:text-[2rem] font-bold">Sign Up</h2>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="name">Name:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="name"
+            placeholder="Name"
+            name="name"
+            value={data.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="email"
-          placeholder="Email address"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-          required
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="email"
+            placeholder="mail@example.com"
+            name="email"
+            value={data.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="password"
-          placeholder="Password"
-          name="password"
-          value={data.password}
-          onChange={handleChange}
-          required
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password">Password:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-        <input
-          className="p-4 md:w-[25rem] border-[1px] border-gray-400"
-          type="password"
-          placeholder="Password confirmation"
-          name="password_confirmation"
-          value={data.password_confirmation}
-          onChange={handleChange}
-          required
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="password_confirmation">Password Confirmation:</label>
+          <input
+            className="p-4 md:w-[25rem] border-[1px] border-gray-400"
+            type="password"
+            placeholder="Password confirmation"
+            name="password_confirmation"
+            value={data.password_confirmation}
+            onChange={handleChange}
+            required
+          />
+        </div>
         <button
           type="submit"
           className="text-white text-[0.9rem] bg-cyan-800 w-fit hover:bg-cyan-700 transition-all duration-300 ease-in-out"
