6d724af3a805288a1426b64a5fead3b0ccbd2128
Kagheni Mbale <kaghenimbale@gmail.com>
2025-07-27 14:45:54 +0200

    Add translation to the hours component

--- app/[locale]/components/Wellness.tsx
+++ app/[locale]/components/Wellness.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
 const Wellness = () => {
+  const t = useTranslations("wellness");
   return (
     <section className="flex flex-col md:flex-row items-center justify-center gap-12 px-4 md:px-10 py-20 bg-white relative z-0">
       {/* Image & Background Box */}
@@ -27,19 +29,16 @@ const Wellness = () => {
       {/* Text Content */}
       <div className="flex flex-col gap-6 md:gap-8 max-w-xl text-center md:text-left">
         <h3 className="text-[1.8rem] md:text-[2.25rem] font-bold text-cyan-950">
-          Wellness & Spa
+          {t("title")}
         </h3>
         <p className="text-gray-600 font-light leading-relaxed">
-          Escape to our serene Wellness & Spa. Discover a haven where
-          tranquility meets rejuvenation. Our expert treatments are designed to
-          soothe your senses, relax your mind, and revitalize your body. Indulge
-          in an experience tailored for your ultimate well-being.
+          {t("description")}
         </p>
         <Link
           href="services"
           className="self-center md:self-start text-white text-sm font-medium bg-cyan-800 px-6 py-2 rounded hover:bg-cyan-700 transition-all duration-300"
         >
-          BOOK NOW
+          {t("link")}
         </Link>
       </div>
     </section>
