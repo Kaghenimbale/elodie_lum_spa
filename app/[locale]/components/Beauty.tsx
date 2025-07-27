5bde3aaea8a3dc64b67be57b4d9913ee975e8404
Kagheni Mbale <kaghenimbale@gmail.com>
2025-07-27 14:00:00 +0200

    Add translation to the beauty component

--- app/[locale]/components/Beauty.tsx
+++ app/[locale]/components/Beauty.tsx
@@ -2,8 +2,15 @@
 
 import Image from "next/image";
 import SpaServices from "../services/SpaServices";
+import { useTranslations } from "next-intl";
 
 const Beauty = () => {
+  const t = useTranslations("beauty");
+  const services = t.raw("services") as {
+    icon: any;
+    title: string;
+    description: string;
+  }[];
   return (
     <section className="py-10 md:py-20 flex flex-col items-center gap-16 md:gap-28 bg-white">
       {/* Heading Section */}
@@ -11,13 +18,7 @@ const Beauty = () => {
         <h2 className="text-[1.8rem] md:text-[2.25rem] font-bold">
           ELODIA BEAUTY & SPA
         </h2>
-        <p className="max-w-3xl text-gray-700 font-thin">
-          Your beauty truly matters to us — it’s at the heart of everything we
-          do. At ELODIA BEAUTY & SPA, we offer a luxurious space where elegance
-          meets comfort. Each detail is designed to help you relax, recharge,
-          and shine. Whether it's your first visit or your tenth, you'll feel
-          right at home. Step into a world of splendor made just for you.
-        </p>
+        <p className="max-w-3xl text-gray-700 font-thin">{t("description")}</p>
       </div>
 
       {/* Services Section */}
@@ -25,26 +26,7 @@ const Beauty = () => {
 
       {/* Features Section */}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12 px-4">
-        {[
-          {
-            icon: "/bol.png",
-            title: "Hair Removal",
-            description:
-              "Say goodbye to unwanted hair with our gentle waxing services. Designed for all skin types, our treatments leave your skin smooth, clean, and glowing for weeks.",
-          },
-          {
-            icon: "/detente.png",
-            title: "Basic Elodia Facial",
-            description:
-              "Refresh your face with our signature facial that combines deep cleansing and warm volcanic stones. Ideal for tired or dull skin, it restores hydration and natural radiance.",
-          },
-          {
-            icon: "/sauna2.png",
-            title: "Massage",
-            description:
-              "Let tension melt away with our customized massages. Whether you're seeking stress relief or muscle recovery, our techniques help you feel lighter, relaxed, and renewed.",
-          },
-        ].map(({ icon, title, description }, index) => (
+        {services.map(({ icon, title, description }, index) => (
           <div
             key={index}
             className="w-full max-w-sm mx-auto flex flex-col items-center text-center gap-6 opacity-80 hover:opacity-100 transition-opacity duration-300"
