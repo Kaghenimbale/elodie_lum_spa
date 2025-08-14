469a828b38c9d3fa8d3fca25ff5adc925be4d130
Kagheni Mbale <kaghenimbale@gmail.com>
2025-08-14 15:37:01 +0200

    Remove comment code in the wellness component

--- app/[locale]/components/Wellness1.tsx
+++ app/[locale]/components/Wellness1.tsx
@@ -7,13 +7,6 @@ import Link from "next/link";
 const Wellness1 = () => {
   const t = useTranslations("wellness1");
   const locale = useLocale();
-  const items = t.raw("items");
-  const services = t.raw("serviceList") as {
-    icon: any;
-    title: string;
-    price: string;
-    desc: string;
-  }[];
 
   return (
     <section className="flex flex-col items-center py-16 px-4 space-y-16 bg-white">
@@ -25,11 +18,6 @@ const Wellness1 = () => {
           <p className="text-gray-700 font-light leading-relaxed">
             {t("description")}
           </p>
-          {/* <ul className="list-disc list-inside text-gray-700 font-light space-y-1">
-            {items.map((item: string, index: number) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul> */}
           <Link
             href="/services"
             locale={locale}
@@ -53,32 +41,6 @@ const Wellness1 = () => {
           <div className="absolute bg-orange-50 -z-0 w-[30rem] h-[25rem] -top-10 rounded-full blur-md"></div>
         </div>
       </div>
-
-      {/* Services grid */}
-      {/* <div className="w-full max-w-7xl grid grid-cols-1 sm:grid-cols-2 gap-8 p-4">
-        {services.map(({ icon, title, price, desc }, i) => (
-          <div
-            key={i}
-            className="flex gap-6 items-start p-4 rounded hover:bg-gray-50 transition"
-          >
-            <Image
-              src={icon}
-              alt={title}
-              width={70}
-              height={70}
-              className="shrink-0"
-            />
-            <div className="flex flex-col gap-2 w-full">
-              <div className="flex justify-between items-center flex-wrap gap-2">
-                <h4 className="text-lg font-semibold text-cyan-900">{title}</h4>
-                <span className="text-sm text-gray-600">from {price}</span>
-              </div>
-              <hr className="border-gray-200 w-full" />
-              <p className="text-sm text-gray-600">{desc}</p>
-            </div>
-          </div>
-        ))}
-      </div> */}
     </section>
   );
 };
